f128fcbed8575e15b6af645c020e3eae18002d2d
djsebas <james.franco.velandia@unillanos.edu.co>
2022-08-05 15:59:55 -0500

    Primer commit

--- php/conexion_postgres.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$conexion=pg_conection("host_localhost dbname=sga user=sebastianpc password=1Sf007790743");
-
-?>
--- php/iniciar_sesion.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require 'conexion_postgres.php';
-
-session_start();
-
-$usuario=$_POST['username'];
-$clave=$_POST['password'];
-
